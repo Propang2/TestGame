d87fca2e63581a4ff8fc0a61736b3bcb86a0d649
Alexander Strandberg <alexander.strandberg@telia.com>
2019-05-02 21:38:53 +0200

    Add project files.

---
+++ TestGame/Enemy.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace TestGame
+{
+    class Enemy
+    {
+        public Texture2D texture;
+        public Vector2 position;
+        public Vector2 speed = new Vector2(5,0);
+        public int radius;
+
+        public static List<Enemy> enemies = new List<Enemy>();
+
+        public Enemy(Texture2D _texture)
+        {
+            texture = _texture;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            position += speed;
+
+            //Detta ger felmeddelande
+            foreach (Drifter d in enemies)
+            {
+                d.Behavior();
+            }
+
+            foreach (Bomber b in enemies)
+            {
+                b.Behavior();
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(texture, position, Color.White);
+            spriteBatch.End();
+        }
+    }
+
+    class Drifter : Enemy
+    {
+        public Drifter(Texture2D t) : base(t)
+        {
+            position = new Vector2(100, 100);
+            radius = 50;
+        }
+
+        public void Behavior()
+        {
+            if (position.X < 0)
+            {
+                speed.X = 5;
+            }
+            else if (position.X > 400)
+            {
+                speed.X = -5;
+            }
+        }
+
+    }
+
+    class Bomber : Enemy
+    {
+        public Bomber(Texture2D t) : base(t)
+        {
+            position = new Vector2(300, 300);
+            radius = 50;
+        }
+
+        public void Behavior()
+        {
+            if (position.X < 0)
+            {
+                speed.X = 5;
+                speed.Y = 2;
+            }
+            else if (position.X > 400)
+            {
+                speed.X = -5;
+                speed.Y = -2;
+            }
+        }
+    }
+}
---
+++ TestGame/Game1.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TestGame
+{
+
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Texture2D drifterSprite;
+        Texture2D bomberSprite;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferHeight = 500;
+            graphics.PreferredBackBufferWidth = 500;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            drifterSprite = Content.Load<Texture2D>("red");
+            bomberSprite = Content.Load<Texture2D>("pink");
+
+            Drifter drifter = new Drifter(drifterSprite);
+            Enemy.enemies.Add(drifter);
+
+            Bomber bomber = new Bomber(bomberSprite);
+            Enemy.enemies.Add(bomber);
+
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            //Hur får jag fiendernas individuella betéende?
+            foreach (Enemy e in Enemy.enemies)
+            {
+                e.Update(gameTime);
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            foreach (Enemy e in Enemy.enemies)
+            {
+                e.Draw(spriteBatch);
+            }
+
+            base.Draw(gameTime);
+        }
+    }
+}
